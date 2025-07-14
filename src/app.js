adff45ee5d227f3b9da35113ae2ab0be0703afa2
kozlov220101@gmail.com <kozlov220101@gmail.com>
2025-07-14 23:51:39 +0500

    Fixes

--- src/app.js
+++ src/app.js
@@ -1,55 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const addTaskModal = document.getElementById('addTaskModal');
-	const closeModalButton = document.querySelector('button.closeModal');
-	const openModalButton = document.querySelector('button.addButton');
-	const addTaskButton = document.querySelector('button.addTask');
-	const toggleThemeButton = document.querySelector('button.toggleTheme');
-	const tasksList = document.querySelector('ul.tasksList');
-	const addTaskInput = document.querySelector('input.addTaskInput');
+	const addTaskModal = document.querySelector('.add-task-modal');
+	const confirmDeleteModal = document.querySelector('.confirm-delete-modal');
+	const closeAddModalButton = document.querySelector('.close-add-modal');
+	const closeDeleteModalButton = document.querySelector('.close-delete-modal');
+	const openModalButton = document.querySelector('.add-button');
+	const addTaskButton = document.querySelector('.add-task');
+	const confirmDeleteButton = document.querySelector('.confirm-delete');
+	const tasksList = document.querySelector('.tasks-list');
+	const addTaskInput = document.querySelector('.add-task-input');
+	const toggleThemeButton = document.querySelector('.toggle-theme');
 	const toggleThemeImage = toggleThemeButton.children[0];
-	const searchInput = document.getElementById('searchInput');
-	const sortSelect = document.querySelector('select.sortType');
-	const allTasksCounter = document.getElementById('allTasksCounter');
-	const activeTasksCounter = document.getElementById('activeTasksCounter');
-	const doneTasksCounter = document.getElementById('doneTasksCounter');
-	const deleteAllSwitchLabel = document.getElementById('deleteAllSwitchLabel');
-	const deleteAllSwitch = document.getElementById('deleteAllSwitch');
-	const container = document.getElementById('container');
-
+	const searchInput = document.querySelector('.search-input');
+	const sortSelect = document.querySelector('.form-select');
+	const allTasksCounter = document.querySelector('.all-tasks-counter');
+	const activeTasksCounter = document.querySelector('.active-tasks-counter');
+	const doneTasksCounter = document.querySelector('.done-tasks-counter');
+	const deleteAllSwitchLabel = document.querySelector('.clear-all-switch');
+	const deleteAllSwitch = document.querySelector('.clear-all-switch-input');
+	const notificationArea = document.querySelector('.notification-area');
 
 	let isDarkTheme = false;
 	let tasks = [];
 
-	const notificationArea = document.getElementById('notificationArea');
 	let pendingDeleteId = null;
 	let notificationTimer = null;
 	let notificationTimeLeft = 5;
 	
 	const tasksURL = 'http://localhost:3000/tasks';
 
+	isDarkTheme = document.documentElement.classList.contains('dark-theme');
+    toggleThemeImage.src = isDarkTheme ? 'assets/images/sun.svg' : 'assets/images/crescent.svg';
+
 	async function initial(){
-		loadTheme();
 		await loadTasks();
 		updateStats();
 		renderTasks();
-		positionFixedButton();
 	}
 
 	function updateStats(){
 		allTasksCounter.textContent = tasks.length;
 		activeTasksCounter.textContent = tasks.filter(task => !task.completed).length;
 		doneTasksCounter.textContent = tasks.filter(task => task.completed).length;
-	}
-
-	/**
-	* Загружает сохраненную тему из localStorage
-	* @returns {void}
-	*/
-	function loadTheme(){
-		const savedTheme = localStorage.getItem('theme');
-		if (savedTheme === 'dark'){
-			toggleTheme();
-		}
 	}
 
 	toggleThemeButton.addEventListener('click', () => {
@@ -61,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	* @returns {void}
 	*/
 	function toggleTheme(){
-		document.body.classList.toggle('dark-theme');
+		document.documentElement.classList.toggle('dark-theme');
 		isDarkTheme = !isDarkTheme;
 		localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
 		toggleThemeImage.src = isDarkTheme ? 'assets/images/sun.svg':'assets/images/crescent.svg';
@@ -107,7 +98,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		tasksList.innerHTML = '';
 		if ( visibleTasks.length === 0 ) {
 
-			tasksList.innerHTML = `<div class="emptyList">
+			tasksList.innerHTML = `<div class="empty-list">
 				<img src="assets/images/empty-list-img.png" alt="Empty list">
 				<span>Empty...</span>
 			</div>`;
@@ -117,30 +108,32 @@ document.addEventListener("DOMContentLoaded", () => {
 		visibleTasks.forEach(task => {
 			const taskEl = document.createElement('li');
 			taskEl.dataset.id = task.id;
-			taskEl.innerHTML = `<input class="form-checkbox taskCheckbox" type="checkbox" name="completed" ${task.completed ? 'checked' : ''}>
-			<span class="taskText">${task.text}</span>
-			<div class="editTask pointer" style="margin-left: auto;">
-				<svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M7.67272 3.49106L1 10.1637V13.5H4.33636L11.0091 6.82736M7.67272 3.49106L10.0654 1.09837L10.0669 1.09695C10.3962 0.767585 10.5612 0.602613 10.7514 0.540824C10.9189 0.486392 11.0993 0.486392 11.2669 0.540824C11.4569 0.602571 11.6217 0.767352 11.9506 1.09625L13.4018 2.54738C13.7321 2.87769 13.8973 3.04292 13.9592 3.23337C14.0136 3.40088 14.0136 3.58133 13.9592 3.74885C13.8974 3.93916 13.7324 4.10414 13.4025 4.43398L13.4018 4.43468L11.0091 6.82736M7.67272 3.49106L11.0091 6.82736" stroke="#CDCDCD" stroke-linecap="round" stroke-linejoin="round"/>
-				</svg>
-			</div>
-			<div class="deleteTask pointer">
-				<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M3.87426 7.61505C3.80724 6.74386 4.49607 6 5.36983 6H12.6302C13.504 6 14.1928 6.74385 14.1258 7.61505L13.6065 14.365C13.5464 15.1465 12.8948 15.75 12.1109 15.75H5.88907C5.10526 15.75 4.4536 15.1465 4.39348 14.365L3.87426 7.61505Z" stroke="#CDCDCD"/>
-					<path d="M14.625 3.75H3.375" stroke="#CDCDCD" stroke-linecap="round"/>
-					<path d="M7.5 2.25C7.5 1.83579 7.83577 1.5 8.25 1.5H9.75C10.1642 1.5 10.5 1.83579 10.5 2.25V3.75H7.5V2.25Z" stroke="#CDCDCD"/>
-					<path d="M10.5 9V12.75" stroke="#CDCDCD" stroke-linecap="round"/>
-					<path d="M7.5 9V12.75" stroke="#CDCDCD" stroke-linecap="round"/>
-				</svg>
+			taskEl.innerHTML = `<input class="form-checkbox task-checkbox" type="checkbox" name="completed" ${task.completed ? 'checked' : ''}>
+			<span class="task-text">${task.text}</span>
+			<div class="task-actions">
+				<div class="edit-task pointer" style="margin-left: auto;">
+					<svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M7.67272 3.49106L1 10.1637V13.5H4.33636L11.0091 6.82736M7.67272 3.49106L10.0654 1.09837L10.0669 1.09695C10.3962 0.767585 10.5612 0.602613 10.7514 0.540824C10.9189 0.486392 11.0993 0.486392 11.2669 0.540824C11.4569 0.602571 11.6217 0.767352 11.9506 1.09625L13.4018 2.54738C13.7321 2.87769 13.8973 3.04292 13.9592 3.23337C14.0136 3.40088 14.0136 3.58133 13.9592 3.74885C13.8974 3.93916 13.7324 4.10414 13.4025 4.43398L13.4018 4.43468L11.0091 6.82736M7.67272 3.49106L11.0091 6.82736" stroke="#CDCDCD" stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+				</div>
+				<div class="delete-task pointer">
+					<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M3.87426 7.61505C3.80724 6.74386 4.49607 6 5.36983 6H12.6302C13.504 6 14.1928 6.74385 14.1258 7.61505L13.6065 14.365C13.5464 15.1465 12.8948 15.75 12.1109 15.75H5.88907C5.10526 15.75 4.4536 15.1465 4.39348 14.365L3.87426 7.61505Z" stroke="#CDCDCD"/>
+						<path d="M14.625 3.75H3.375" stroke="#CDCDCD" stroke-linecap="round"/>
+						<path d="M7.5 2.25C7.5 1.83579 7.83577 1.5 8.25 1.5H9.75C10.1642 1.5 10.5 1.83579 10.5 2.25V3.75H7.5V2.25Z" stroke="#CDCDCD"/>
+						<path d="M10.5 9V12.75" stroke="#CDCDCD" stroke-linecap="round"/>
+						<path d="M7.5 9V12.75" stroke="#CDCDCD" stroke-linecap="round"/>
+					</svg>
+				</div>
 			</div>`;
 
 			const taskCheckbox = taskEl.querySelector('input.form-checkbox');
 			taskCheckbox.addEventListener('change', () => toggleCompleteTask(task.id));
 
-			const taskEditButton = taskEl.querySelector('div.editTask');
+			const taskEditButton = taskEl.querySelector('div.edit-task');
 			taskEditButton.addEventListener('click', () => editTask(taskEl, task));
 
-			const taskDeleteButton = taskEl.querySelector('div.deleteTask');
+			const taskDeleteButton = taskEl.querySelector('div.delete-task');
 			taskDeleteButton.addEventListener('click', () => startTaskDeletion(task.id));
 
 			tasksList.appendChild(taskEl);
@@ -189,10 +182,10 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 
 	function editTask(taskEl, task){
-		const taskText = taskEl.querySelector('span.taskText');
+		const taskText = taskEl.querySelector('span.task-text');
 		const editTaskInput = document.createElement('input');
 		editTaskInput.type = 'text';
-		editTaskInput.className = 'form-input taskEditInput';
+		editTaskInput.className = 'form-input task-edit-input';
 		editTaskInput.value = task.text;
 
 		taskText.replaceWith(editTaskInput);
@@ -210,6 +203,10 @@ document.addEventListener("DOMContentLoaded", () => {
 	}
 
 	async function finishEditTask(task, text){
+		if (text.trim() === task.text) {
+			renderTasks();
+			return;
+		}
 		if (text.trim() !== '') {
 			task.text = text.trim();
 			await saveTaskToServer(task);
@@ -267,7 +264,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		localStorage.setItem('tasks', JSON.stringify(fetchedTasks));
 	}
 
-	async function deleteAllTasks(){
+	function deleteAllTasks(){
 		if (!tasks.length){
 			deleteAllSwitch.checked = false;
 			deleteAllSwitchLabel.classList.add('shake');
@@ -276,17 +273,21 @@ document.addEventListener("DOMContentLoaded", () => {
 			}, 600);
 			return;
 		} else {
-			await axios.delete(tasksURL + '/clearTasks');
-			tasks = [];
-			updateStats();
-			renderTasks();
-			updateLocalStorage();
-			showAlert("All tasks are deleted" ,'success');
+			openModal(confirmDeleteModal);
 			setTimeout(()=>{
 				deleteAllSwitch.checked = false;
 			}, 600);
 		}
+	}
 
+	async function confirmDeletion() {
+		await axios.delete(tasksURL + '/clearTasks');
+		tasks = [];
+		closeModal(confirmDeleteModal);
+		updateStats();
+		renderTasks();
+		updateLocalStorage();
+		showAlert("All tasks are deleted" ,'success');
 	}
 
 	/**
@@ -378,8 +379,12 @@ document.addEventListener("DOMContentLoaded", () => {
 		openModal(addTaskModal);
 	});
 
-	closeModalButton.addEventListener("click", () => {
+	closeAddModalButton.addEventListener("click", () => {
 		closeModal(addTaskModal);
+	})
+
+	closeDeleteModalButton.addEventListener("click", () => {
+		closeModal(confirmDeleteModal);
 	})
 
 	addTaskButton.addEventListener("click", () => {
@@ -389,6 +394,16 @@ document.addEventListener("DOMContentLoaded", () => {
 	addTaskInput.addEventListener("keypress", (e) => {
 		if (e.key === 'Enter') {
 			addTask();
+		}
+	})
+
+	confirmDeleteButton.addEventListener("click", () => {
+		confirmDeletion();
+	})
+
+	confirmDeleteButton.addEventListener("keypress", (e) => {
+		if (e.key === 'Enter') {
+			confirmDeletion();
 		}
 	})
 
@@ -406,13 +421,19 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	});
 
-	function positionFixedButton(){
-		openModalButton.style.left = container.offsetLeft + container.clientWidth - openModalButton.clientWidth + 'px';
-	}
-
-	window.addEventListener('resize', () => {
-		positionFixedButton();
-	})
+	addTaskModal.addEventListener('click', (event) => {
+		const modalContainer = addTaskModal.querySelector('.modal-container');
+		if (!modalContainer.contains(event.target)) {
+			closeModal(addTaskModal);
+		}
+	});
+	
+	confirmDeleteModal.addEventListener('click', (event) => {
+		const modalContainer = confirmDeleteModal.querySelector('.modal-container');
+		if (!modalContainer.contains(event.target)) {
+			closeModal(confirmDeleteModal);
+		}
+	});
 
 	function showAlert(message, type) {
 
